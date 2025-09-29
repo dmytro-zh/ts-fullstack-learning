3be9bea6a3aa4119189e283b35048e16256856d1
Dmytro Zhuravlov <dima9696.91@gmail.com>
2025-09-29 17:16:56 -0400

    feat(api): add Prisma schema and SQLite datasource

---
+++ apps/api/prisma/migrations/20250929201957_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "inStock" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
