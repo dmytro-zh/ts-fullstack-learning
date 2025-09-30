da69b9ad92d5aed0b85928721976b85e03b6ec65
Dmytro Zhuravlov <dima9696.91@gmail.com>
2025-09-30 18:12:21 -0400

    feat(web): add server action for addProduct mutation

---
+++ apps/web/src/app/actions/addProduct.ts
@@ -0,0 +1,11 @@
+'use server';
+
+import { GraphQLClient } from 'graphql-request';
+import { getEnv } from '../lib/env';
+import { AddProductDocument, type AddProductMutationVariables } from '../graphql/generated/graphql';
+
+export async function addProductAction(input: AddProductMutationVariables) {
+  const { GRAPHQL_URL } = getEnv();
+  const client = new GraphQLClient(GRAPHQL_URL);
+  await client.request(AddProductDocument, input);
+}
