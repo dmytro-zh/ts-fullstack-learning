6da195366e0bd5f4c4fc2fe4f9ab38c35c0be1ca
Dmytro Zhuravlov <dima9696.91@gmail.com>
2025-09-30 18:24:32 -0400

    feat(web): link to product creation page

--- apps/web/src/app/actions/addProduct.ts
+++ apps/web/src/app/actions/addProduct.ts
@@ -1,8 +1,8 @@
 'use server';
 
 import { GraphQLClient } from 'graphql-request';
-import { getEnv } from '../lib/env';
-import { AddProductDocument, type AddProductMutationVariables } from '../graphql/generated/graphql';
+import { getEnv } from '../../lib/env'; 
+import { AddProductDocument, type AddProductMutationVariables } from '../../graphql/generated/graphql';
 
 export async function addProductAction(input: AddProductMutationVariables) {
   const { GRAPHQL_URL } = getEnv();
