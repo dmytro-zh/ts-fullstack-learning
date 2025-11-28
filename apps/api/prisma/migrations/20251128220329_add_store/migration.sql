8368d41a15ad02b9b3432a5bf3ccd96b716750c4
dmytro-zh <dima9696.91@gmail.com>
2025-11-28 17:08:06 -0500

    feat(api): add store model and product relation (#29)

---
+++ apps/api/prisma/migrations/20251128220329_add_store/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Store" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "inStock" BOOLEAN NOT NULL DEFAULT true,
+    "storeId" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Product_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Product" ("createdAt", "id", "inStock", "name", "price", "updatedAt") SELECT "createdAt", "id", "inStock", "name", "price", "updatedAt" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
